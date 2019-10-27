85416de265258c223ff7c75b07fb5dc3494b3ede
James Smith <james.smith@djalbat.com>
2019-10-27 15:53:23 +0000

    Incorporated the latest lexers and parsers and made the appropriate changes.

--- es6/css/lexer.js
+++ es6/css/lexer.js
@@ -9,7 +9,15 @@ const { CommonLexer, EndOfLineSignificantToken } = lexers;
 class CSSLexer extends CommonLexer {
   tokeniseEndOfLines(content) { return super.tokeniseEndOfLines(content, EndOfLineSignificantToken); }
 
-  tokeniseRegularExpressions(tokensOrContents) {}
+  matchBrokenComment(content, inComment) { return null; }
+
+  matchSingleLineComment(content, inComment) { return null; }
+
+  matchMultiLineCommentInComment(content, inComment) { return null; }
+
+  matchMultiLineCommentNotInComment(content, inComment) { return null; }
+
+  matchRegularExpression(content) { return null; }
 
   static fromEntries(entries) { return CommonLexer.fromEntries(CSSLexer, entries); }
 
