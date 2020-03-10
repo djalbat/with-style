05eb4aceacc66dea2d5b953a914d4930595faf16
James Smith <james.smith@djalbat.com>
2020-03-10 13:32:06 +0000

    Transpiled the index file.

---
+++ es6/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import tagNames from '../lib/tagNames';
+import classUtilities from '../lib/utilities/class';
+import stylesUtilities from '../lib/utilities/styles';
+import classNameUtilities from '../lib/utilities/className';
+
+export {
+  tagNames,
+  classUtilities,
+  stylesUtilities,
+  classNameUtilities
+};
