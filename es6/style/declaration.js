9b8a4d6e7173b7244b2c2d21832c793ad741404b
James Smith <james.smith@djalbat.com>
2020-05-09 20:08:42 +0100

    Split declarations into properties and expressions.

--- es6/style/declaration.js
+++ es6/style/declaration.js
@@ -1,28 +1,52 @@
 "use strict";
 
+import { Query } from "occam-dom";
+import { arrayUtilities } from "necessary";
+
 import { asContent } from "../utilities/node";
 
+const { first } = arrayUtilities;
+
+const propertyQuery = Query.fromExpression("/*/property"),
+      expressionQuery = Query.fromExpression("/*/expression");
+
 export default class Declaration {
-  constructor(content) {
-    this.content = content;
+  constructor(property, expression) {
+    this.property = property;
+    this.expression = expression;
   }
 
   getContent() {
-    return this.content;
+    return this.property;
+  }
+
+  getExpression() {
+    return this.expression;
   }
 
   asCSS(indent, last) {
     const css = last ?
-                `${indent}${this.content}` :
-                  `${indent}${this.content}\n`;
+                `${indent}${this.property}: ${this.expression};` :
+                  `${indent}${this.property}: ${this.expression};\n`;
 
     return css;
   }
 
   static fromNodeAndTokens(node, tokens) {
-    const content = asContent(node, tokens),
-          declaration = new Declaration(content);
+    const propertyContent = contentFromQueryNodeAndTokens(propertyQuery, node, tokens),
+          expressionContent = contentFromQueryNodeAndTokens(expressionQuery, node, tokens),
+          property = propertyContent, ///
+          expression = expressionContent, ///
+          declaration = new Declaration(property, expression);
 
     return declaration;
   }
+}
+
+function contentFromQueryNodeAndTokens(query, node, tokens) {
+  const nodes = query.execute(node),
+        firstNode = first(nodes),
+        content = asContent(firstNode, tokens);
+
+  return content;
 }
