b3e41aae91e201e69118e85954339590202eef8a
James Smith <james.smith@djalbat.com>
2020-05-09 22:50:50 +0100

    Simplified the class name generation.

--- es6/constants.js
+++ es6/constants.js
@@ -1,5 +1,4 @@
 "use strict";
 
-export const ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+export const CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 export const CLASS_NAME_LENGTH = 6;
-export const RANDOM_NUMBER_MAXIMUM = 1000000000;
--- es6/utilities/className.js
+++ es6/utilities/className.js
@@ -1,23 +1,25 @@
 "use strict";
 
 import uuidV4 from "uuid/v4";
-import Hashids from "hashids";
 import randomSeed from "random-seed";
 
-import { ALPHABET, CLASS_NAME_LENGTH, RANDOM_NUMBER_MAXIMUM } from "../constants";
+import { CHARACTERS, CLASS_NAME_LENGTH } from "../constants";
 
 const uuid = uuidV4(), ///
       random = randomSeed.create(uuid),
       length = CLASS_NAME_LENGTH,
-      alphabet = ALPHABET,
-      hashids = new Hashids("", length, alphabet),
-      randomNumberMaximum = RANDOM_NUMBER_MAXIMUM;
-
-let randomNumber = random(randomNumberMaximum);
+      characters = CHARACTERS,
+      charactersLength = characters.length;
 
 export function generateClassName() {
-  const hash = hashids.encode(randomNumber++),
-        className = hash; ///
+  let className = "";
+
+  for (let count = 0; count < length; count++) {
+    const index = random(charactersLength),
+          character = characters[index];
+
+    className += character;
+  }
 
   return className;
 }
