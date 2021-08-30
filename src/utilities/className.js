7842aef616b57fbc1479d6d580cb5850fc86961e
James Smith <james.smith@djalbat.com>
2021-08-30 10:52:11 +0100

    Removed the external packages and simplified the class name generation as a consequence.

--- src/utilities/className.js
+++ src/utilities/className.js
@@ -1,25 +1,30 @@
 "use strict";
 
-import uuidV4 from "uuid/v4";
-import randomSeed from "random-seed";
-
 import { CHARACTERS, EMPTY_STRING, CLASS_NAME_LENGTH } from "../constants";
 
-const uuid = uuidV4(), ///
-      random = randomSeed.create(uuid),
-      length = CLASS_NAME_LENGTH,
+const length = CLASS_NAME_LENGTH,
       characters = CHARACTERS,
       charactersLength = characters.length;
+
+const classNames = [];
 
 export function generateClassName() {
   let className = EMPTY_STRING;
 
-  for (let count = 0; count < length; count++) {
-    const index = random(charactersLength),
+  for (let index = 0; index < length; index++) {
+    const index = Math.floor(Math.random() * charactersLength),
           character = characters[index];
 
     className += character;
   }
+
+  const classNamesIncludesClassName = classNames.includes(className);
+
+  if (classNamesIncludesClassName) {
+    className = generateClassName();
+  }
+
+  classNames.push(className);
 
   return className;
 }
