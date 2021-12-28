92d865fa6650ae2c64757e0bd20d7ff430ba575c
James Smith <james.smith@djalbat.com>
2021-12-28 19:05:20 +0000

    Incorporated the latest lexers.

--- src/css/lexer.js
+++ src/css/lexer.js
@@ -1,48 +1,15 @@
 "use strict";
 
-import { CommonLexer,
-         SingleLineCommentToken,
-         EndOfMultiLineCommentToken,
-         EndOfLineNonSignificantToken,
-         StartOfMultiLineCommentToken,
-         MiddleOfMultiLineCommentToken } from "occam-lexers";
+import { CommonLexer } from "occam-lexers";
 
 import entries from "./entries";
 
 export default class CSSLexer extends CommonLexer {
-  tokeniseEndOfLines(content) { return super.tokeniseEndOfLines(content, EndOfLineNonSignificantToken); }
-
-  matchBrokenComment(content, inComment) { return null; }
-
-  matchSingleLineComment(content, inComment) {
-    const singleLineCommentToken = inComment ?
-                                     null :
-                                       SingleLineCommentToken.match(content);
-
-    return singleLineCommentToken;
-  }
-
-  matchMultiLineCommentInComment(content, inComment) {
-    const multiLinCommentToken = inComment ?
-                                   EndOfMultiLineCommentToken.match(content) || MiddleOfMultiLineCommentToken.match(content) :
-                                     null;
-
-    return multiLinCommentToken;
-  }
-
-  matchMultiLineCommentNotInComment(content, inComment) {
-    const multiLinCommentToken = inComment ?
-                                   null :
-                                     StartOfMultiLineCommentToken.match(content);
-
-    return multiLinCommentToken;
-  }
-
-  matchRegularExpression(content) { return null; }
-
   static entries = entries;
 
-  static fromEntries(entries) { return CommonLexer.fromEntries(CSSLexer, entries); }
+  static RegularExpressionToken = null;
 
   static fromNothing() { return CommonLexer.fromNothing(CSSLexer); }
+
+  static fromEntries(entries) { return CommonLexer.fromEntries(CSSLexer, entries); }
 }
