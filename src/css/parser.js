b448ed4cdc3e6c4c702fb628c57181b2c4f616eb
James Smith <james.smith@djalbat.com>
2022-01-09 16:57:21 +0000

    Incorporated the latest parsers.

--- src/css/parser.js
+++ src/css/parser.js
@@ -1,25 +1,15 @@
 "use strict";
 
-import { BNFLexer } from "occam-lexers";
-import { BNFParser, CommonParser } from "occam-parsers";
+import { CommonParser } from "occam-parsers";
 
 import bnf from "./bnf";
-
-const bnfLexer = BNFLexer.fromNothing(),
-      bnfParser = BNFParser.fromNothing();
 
 export default class CSSParser extends CommonParser {
   static bnf = bnf;
 
-  static fromBNF(bnf) {
-    const tokens = bnfLexer.tokensFromBNF(bnf),
-          rules = bnfParser.rulesFromTokens(tokens),
-          cssParser = CSSParser.fromRules(rules);
+  static fromNothing() { return CommonParser.fromNothing(CSSParser); }
 
-    return cssParser;
-  }
+  static fromBNF(bnf) { return CommonParser.fromBNF(CSSParser, bnf); }
 
   static fromRules(rules) { return CommonParser.fromRules(CSSParser, rules); }
-
-  static fromNothing() { return CSSParser.fromBNF(bnf); }
 }
