cfb786cdcf4338dd428e24e0dfaa7c93703120e6
James Smith <james.smith@djalbat.com>
2022-02-03 19:50:18 +0000

    Gotten the new lexer dependency working.

--- src/css/lexer.js
+++ src/css/lexer.js
@@ -1,13 +1,29 @@
 "use strict";
 
-import { CommonLexer } from "occam-lexers";
+import { CommonLexer, WhitespaceToken, EndOfLineNonSignificantToken, DoublyQuotedStringLiteralToken } from "occam-lexers";
 
 import entries from "./entries";
 
 export default class CSSLexer extends CommonLexer {
   static entries = entries;
 
+  static EndOfLineToken = EndOfLineNonSignificantToken; ///
+
+  static WhitespaceToken = WhitespaceToken;
+
   static RegularExpressionToken = null;
+
+  static SingleLineCommentToken = null; ///
+
+  static EndOfMultiLineCommentToken = null; ///
+
+  static StartOfMultiLineCommentToken = null; ///
+
+  static MiddleOfMultiLineCommentToken = null; ///
+
+  static SinglyQuotedStringLiteralToken = null;
+
+  static DoublyQuotedStringLiteralToken = DoublyQuotedStringLiteralToken;
 
   static fromNothing() { return CommonLexer.fromNothing(CSSLexer); }
 
